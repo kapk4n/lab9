303202de3ed4ec6cf0c8dfb7d7a5e481c7a9521d
kapk4n <kapkan.play.csgo@icloud.com>
2022-12-02 21:29:53 +0300

    final lab8

--- app/controllers/example_controller.rb
+++ app/controllers/example_controller.rb
@@ -1,8 +1,34 @@
+# frozen_string_literal: true
+
+# created class
 class ExampleController < ApplicationController
-  def input
-  end
+  before_action :check_par, only: [:show]
+
+  def input; end
 
   def show
-    @digit = params[:myParam]
+    @result = vichsl(params[:myParam].to_f)
+  end
+
+  private
+
+  def vichsl(dig)
+    @a = []
+    @x0 = dig
+    @x1 = @x0 + 1
+    while ((@x1**2 - dig) / dig) >= 0.001
+      extra = @x1
+      @x0 = extra
+      @x1 = 0.5 * (@x0 + dig / @x0)
+      @a.push(@x1)
+    end
+    @a
+  end
+
+  def check_par
+    return if params[:myParam]&.match(/^[1-9]\d*$/)
+
+    flash[:alert] = 'no match'
+    redirect_to root_path
   end
 end
--- spec/requests/example_spec.rb
+++ spec/requests/example_spec.rb
@@ -1,18 +1,68 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
+require 'nokogiri'
 
-RSpec.describe "Examples", type: :request do
-  describe "GET /input" do
-    it "returns http success" do
-      get "/example/input"
+# rubocop:disable Metrics/BlockLength
+RSpec.describe 'Examples', type: :request do
+  describe 'GET /input' do
+    it 'returns http success' do
+      get root_path
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /show" do
-    it "returns http success" do
-      get "/example/show"
-      expect(response).to have_http_status(:success)
+  describe '/input' do
+    context 'path' do
+      it 'should return http success' do
+        get root_path
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    describe 'GET job' do
+      it 'renders the jobs view' do
+        get example_show_path, params: { myParam: 25 }
+        expect(response).to render_template('example/show')
+      end
+    end
+
+    describe '/show' do
+      context 'path' do
+        it 'should return http success' do
+          get example_show_path, params: { myParam: 25 }
+          expect(response).to have_http_status(:success)
+        end
+      end
+    end
+
+    context 'error' do
+      it 'nil par' do
+        get example_show_path
+        expect(response).to have_http_status(302)
+      end
+
+      it 'empty par' do
+        get example_show_path, params: {}
+        expect(response).to have_http_status(302)
+      end
+    end
+
+    context 'norm' do
+      it 'sqrt 25 = 5' do
+        get example_show_path, params: { myParam: 25 }
+        html = Nokogiri::HTML(response.body)
+        div = html.search('td').last.text
+        expect(div.to_f).to be_within(5 - 0.001).of(5 + 0.001)
+      end
+
+      it 'sqrt 64 = 8' do
+        get example_show_path, params: { myParam: 64 }
+        html = Nokogiri::HTML(response.body)
+        div = html.search('td').last.text
+        expect(div.to_f).to be_within(8 - 0.001).of(8 + 0.001)
+      end
     end
   end
-
 end
+# rubocop:enable Metrics/BlockLength
