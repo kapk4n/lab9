e3c3685d4cf21e7a45e981ea3e3fb2a3d8af2283
kapk4n <kapkan.play.csgo@icloud.com>
2022-12-05 17:18:50 +0300

    lab12 no tests + unique

---
+++ db/migrate/20221205141645_removeuniqind_from_users.rb
@@ -0,0 +1,4 @@
+class RemoveuniqindFromUsers < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_05_124620) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_05_141655) do
   create_table "cars", force: :cascade do |t|
     t.string "brand"
     t.string "model"
@@ -35,6 +35,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_05_124620) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "cars", "drivers"
