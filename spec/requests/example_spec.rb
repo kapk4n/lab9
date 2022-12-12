12f6b4c3cd9f6f844268bfa6eb0d4cbbe4048243
kapk4n <kapkan.play.csgo@icloud.com>
2022-12-12 19:31:45 +0300

    pre-final with tests

--- spec/requests/example_spec.rb
+++ spec/requests/example_spec.rb
@@ -4,65 +4,39 @@ require 'rails_helper'
 require 'nokogiri'
 
 # rubocop:disable Metrics/BlockLength
-RSpec.describe 'Examples', type: :request do
-  describe 'GET /input' do
-    it 'returns http success' do
-      get root_path
-      expect(response).to have_http_status(:success)
-    end
+RSpec.describe 'checking func', type: :system do
+  scenario 'Register, login, calculate' do
+    visit new_user_path
+    fill_in :user_email, with: '123'
+    fill_in :user_password, with: '123'
+    fill_in :user_password_confirmation, with: '123'
+    find('#submit-btn').click
+    expect(current_path).to eq new_user_path
+
+    visit root_path
+    fill_in :email, with: '123'
+    fill_in :password, with: '123'
+    find('#submit-btn').click
+    expect(current_path).to eq root_path
+
+    fill_in :myParam, with: 25
+    find('#submit-btn').click
+    expect(current_path).to eq '/example/input'
+
+    expect(find('#res').text.to_f).to be_within(5 - 0.001).of(5 + 0.001)
   end
 
-  describe '/input' do
-    context 'path' do
-      it 'should return http success' do
-        get root_path
-        expect(response).to have_http_status(:success)
-      end
-    end
+  # def to_path(path)
+  #   visit path
+  #   expect(current_path).to eq session_login_path
+  # end
 
-    describe 'GET job' do
-      it 'renders the jobs view' do
-        get example_show_path, params: { myParam: 25 }
-        expect(response).to render_template('example/show')
-      end
-    end
-
-    describe '/show' do
-      context 'path' do
-        it 'should return http success' do
-          get example_show_path, params: { myParam: 25 }
-          expect(response).to have_http_status(:success)
-        end
-      end
-    end
-
-    context 'error' do
-      it 'nil par' do
-        get example_show_path
-        expect(response).to have_http_status(302)
-      end
-
-      it 'empty par' do
-        get example_show_path, params: {}
-        expect(response).to have_http_status(302)
-      end
-    end
-
-    context 'norm' do
-      it 'sqrt 25 = 5' do
-        get example_show_path, params: { myParam: 25 }
-        html = Nokogiri::HTML(response.body)
-        div = html.search('td').last.text
-        expect(div.to_f).to be_within(5 - 0.001).of(5 + 0.001)
-      end
-
-      it 'sqrt 64 = 8' do
-        get example_show_path, params: { myParam: 64 }
-        html = Nokogiri::HTML(response.body)
-        div = html.search('td').last.text
-        expect(div.to_f).to be_within(8 - 0.001).of(8 + 0.001)
-      end
-    end
-  end
+  # scenario 'Check without register' do
+  #   User.create(username: 'NotHim', password_digest: 'Pass')
+  #   routes = ['/users/1', '/users/1/edit', '/index/output', root_path, '/users']
+  #   routes.each do |path|
+  #     to_path(path)
+  #   end
+  # end
 end
 # rubocop:enable Metrics/BlockLength
